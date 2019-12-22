e343e72419d1a12e0f25790a86fe8e9e7d5fbaeb
azmirizkifar20 <m.azmirizkifar20@gmail.com>
2019-12-22 22:41:31 +0700

    Adding controller method

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+const bootcamps = require('./routes/bootcamps');
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 // load env vars
 dotenv.config({ path: './config/config.env' });
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 app.listen(
     PORT,
