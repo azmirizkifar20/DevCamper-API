9e3b132701a31126f2cb07396f8921f04b697060
azmirizkifar20 <m.azmirizkifar20@gmail.com>
2019-12-23 13:59:20 +0700

    Adding models and Create Read

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,25 +1,55 @@
+const Bootcamp = require('../models/bootcamps');
+
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+
+        res.status(200).json({ success: true, data: bootcamps });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 };
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: `Get bootcamp ${req.params.id}`
-    });
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if (!bootcamp) {
+            return res.status(404).json({
+                success: false,
+                data: `No bootcamp found on ID: ${req.params.id}`
+            });
+        }
+
+        res.status(200).json({ success: true, data: bootcamp });
+    } catch (err) {
+        res.status(404).json({
+            success: false,
+            data: `No bootcamp found on ID: ${req.params.id}`
+        });
+    }
 };
 
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Create new bootcamps' });
+exports.createBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.create(req.body);
+
+        res.status(201).json({
+            success: true,
+            data: bootcamp
+        });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 };
 
 // @desc    Update bootcamp
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // load env vars
 dotenv.config({ path: './config/config.env' });
+
+// connect to database
+connectDB();
+
+// Body Parser
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -16,9 +24,17 @@ if (process.env.NODE_ENV === 'development') {
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-app.listen(
+const server = app.listen(
     PORT,
     console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+            .bold
     )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
